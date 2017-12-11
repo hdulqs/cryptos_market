d873f7a25e6c40822fcd4df557237ec27b858de1
Olivier Ducroux <olivier@dmates.io>
2017-12-11 15:31:42 +0100

    iiii

--- app/services/rest_exchange/assets/fetcher.rb
+++ app/services/rest_exchange/assets/fetcher.rb
@@ -5,7 +5,12 @@ class RestExchange::Assets::Fetcher < RestExchange::Base
   end
 
   def call
-    response_payload = perform_request(@exchange.base_url, @exchange.get_assets_path)
+    begin
+      response_payload = perform_request(@exchange.base_url, @exchange.get_assets_path)
+    rescue
+      sleep(2)
+      retry
+    end
     assets_array = RestExchange::Assets::Adapter.new(@exchange, response_payload).call
     exchange_assets_count = RestExchange::Assets::Persister.new(@exchange, assets_array).call
     return exchange_assets_count
--- app/services/rest_exchange/pairs/fetcher.rb
+++ app/services/rest_exchange/pairs/fetcher.rb
@@ -5,7 +5,12 @@ class RestExchange::Pairs::Fetcher < RestExchange::Base
   end
 
   def call
-    response_payload = perform_request(@exchange.base_url, @exchange.get_pairs_path)
+    begin
+      response_payload = perform_request(@exchange.base_url, @exchange.get_pairs_path)
+    rescue
+      sleep(2)
+      retry
+    end
     pairs_array = RestExchange::Pairs::Adapter.new(@exchange, response_payload).call
     exchange_pairs_count = RestExchange::Pairs::Persister.new(@exchange, pairs_array).call
     return exchange_pairs_count
