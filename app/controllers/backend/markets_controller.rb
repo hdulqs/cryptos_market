2659433df071b5e1897397d0e6472fc9b7714413
Olivier Ducroux <olivier@dmates.io>
2017-12-17 20:05:57 +0100

    ordering by spread

--- app/controllers/backend/markets_controller.rb
+++ app/controllers/backend/markets_controller.rb
@@ -2,7 +2,7 @@ class Backend::MarketsController < Backend::BaseController
 
 
   def index
-    @markets = Market.of_interest.order(price_difference: :desc).where.not(price_difference: [100, 0]).limit(150)
+    @markets = Market.of_interest.order(spread: :desc).where.not(price_difference: [100, 0]).limit(150)
       # Market
       #   .left_joins(:pairs)
       #   .group(:id)
