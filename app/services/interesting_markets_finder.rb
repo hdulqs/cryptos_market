1e25adf083903e3b6602cf1a509e7c32e4833166
Olivier Ducroux <olivier@dmates.io>
2018-01-05 12:47:13 +0100

    iiii

--- app/services/interesting_markets_finder.rb
+++ app/services/interesting_markets_finder.rb
@@ -1,8 +1,15 @@
 class InterestingMarketsFinder
 
   def call
-    markets = Market.left_joins(:pairs).group(:id).order('COUNT(pairs.id) DESC').limit(300)
+    # markets = Market.left_joins(:pairs).group(:id).order('COUNT(pairs.id) DESC').limit(300)
+    markets = Market.where(base_currency: "ETH")
+                    .or(Market.where(quote_currency: "ETH"))
+                    .left_joins(:pairs)
+                    .group(:id)
+                    .order('COUNT(pairs.id) DESC')
+                    .limit(300)
     markets.each do |market|
+      return if market.pairs.count < 2
       market.is_watched = true
       market.save!
       market.pairs.each do |pair|
