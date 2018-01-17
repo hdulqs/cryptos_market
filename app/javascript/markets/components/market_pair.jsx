bb3d6aec6b49997f2b67b16cc91603bac3ee72d2
Olivier Ducroux <olivier@dmates.io>
2018-01-18 00:14:32 +0100

    style

--- app/javascript/markets/components/market_pair.jsx
+++ app/javascript/markets/components/market_pair.jsx
@@ -4,21 +4,15 @@ import { Col } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom'
 import history from './../../main/history'
 
-const styles = {
-  red_flash: {
-    color: 'red' //'#efff00'
-  }
-}
-
 const MarketPair = (props) => {
 
   const navigateToShowMarket = () => {
     history.push('/' + props.pair.exchange_name + '/' + props.pair.last_ticker.market_name)
   }
 
-  let css = props.pair.last_to_be_updated ? styles.red_flash : {}
+  let css_ticker = props.pair.last_to_be_updated ? 'ticker_row' : ''
   return(
-    <tr key={props.pair.id} style={css} onClick={navigateToShowMarket}>
+    <tr key={props.pair.id} className={css_ticker} onClick={navigateToShowMarket}>
       <td>{props.pair.exchange_name + props.pair.id}</td>
       <td>{props.pair.last_ticker.last}</td>
       <td>{props.pair.last_ticker.volume}</td>
