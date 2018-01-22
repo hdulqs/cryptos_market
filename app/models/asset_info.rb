09fb497896bb9eed67d16aa5b238e7250a45242a
Olivier Ducroux <olivier@dmates.io>
2018-01-22 23:16:31 +0100

    fix loader

--- app/models/asset_info.rb
+++ app/models/asset_info.rb
@@ -1,4 +1,7 @@
 class AssetInfo < ApplicationRecord
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "20x20>" }
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "20x20>" }#,
+    # convert_options: {
+    #   thumb: "-quality 100 -thumbnail 20x20"
+    # }
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 end
