9bf2df9cac4b4cb3282a595201853b023fad0ba8
Olivier Ducroux <olivier@dmates.io>
2018-01-26 23:00:20 +0100

    react-ga

--- app/javascript/main/index.js
+++ app/javascript/main/index.js
@@ -12,32 +12,22 @@ import AssetShow from '../assets/components/asset_show'
 import MarketShow from '../markets/components/market_show'
 import history from './history'
 import ReactGA from 'react-ga'
-
-ReactGA.initialize('UA-33195925-2')
-
-const ga_tracking = () => {
-  ReactGA.pageview(window.location.hash)
-}
+import withTracker from './with_tracker'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={store}>
-      <Router onUpdate={ga_tracking} history={history}>
+      <Router history={history}>
         <Switch>
-          <Route exact path='/' component={AssetsApp} />
-          <Route exact path='/asset-pairs' component={MarketsApp} />
-          <Route exact path='/exchanges' component={ExchangesApp} />
-          <Route exact path='/about' component={AboutApp} />
-          <Route exact path='/:symbol' component={AssetShow} />
-          <Route path='/:exchange_name/:market' component={MarketShow} />
+          <Route exact path='/' component={withTracker(AssetsApp)} />
+          <Route exact path='/asset-pairs' component={withTracker(MarketsApp)} />
+          <Route exact path='/exchanges' component={withTracker(ExchangesApp)} />
+          <Route exact path='/about' component={withTracker(AboutApp)} />
+          <Route exact path='/:symbol' component={withTracker(AssetShow)} />
+          <Route path='/:exchange_name/:market' component={withTracker(MarketShow)} />
         </Switch>
       </Router>
     </Provider>,
     document.body.appendChild(document.createElement('div'))
   )
 })
-
-
-/*<Route path='/markets/:market' render={({ match }) => (
-  <MarketShow market={match.params.market.name} />
-)} />*/
