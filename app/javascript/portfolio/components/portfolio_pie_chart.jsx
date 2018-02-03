723add8f7408f4d4eff088f68337cdf869a04b5f
Olivier Ducroux <olivier@dmates.io>
2018-02-03 23:29:27 +0100

    sorting

--- app/javascript/portfolio/components/portfolio_pie_chart.jsx
+++ app/javascript/portfolio/components/portfolio_pie_chart.jsx
@@ -52,7 +52,7 @@ const renderActiveShape = (props) => {
       />
       <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
       <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="white">{`${payload.name} : ${value.toFixed(2)} USD`}</text>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="white">{`$ ${value.toFixed(2)}`}</text>
       <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
         {`${(percent * 100).toFixed(1)}%`}
       </text>
@@ -77,7 +77,6 @@ class TwoLevelPieChart extends Component {
 	render () {
     const renderLegend = (props) => {
       const { payload } = props;
-
       return (
         <ul>
         {
