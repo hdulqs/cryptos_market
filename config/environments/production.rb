89ec8cb4ee4758828806524635291f999255f07a
Olivier Ducroux <olivier@dmates.io>
2018-02-08 02:21:10 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { :host => 'cryptosmarket.io' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
