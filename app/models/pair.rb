e414d18245198fd9aca20ed68869ad51831b174f
Olivier Ducroux <olivier@dmates.io>
2018-02-09 00:47:35 +0100

    tickers

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -7,11 +7,11 @@ class Pair < ApplicationRecord
 
   scope :active, -> { where(is_active: true) }
   scope :watched, -> { where(is_watched: true) }
-  scope :with_recent_ticker, -> {
-    joins(:tickers).where(
-      tickers: { created_at: (Time.now - 10.minutes)..Time.now }
-    )
-  }
+  # scope :with_recent_ticker, -> {
+  #   joins(:tickers).where(
+  #     tickers: { created_at: (Time.now - 10.minutes)..Time.now }
+  #   )
+  # }
   scope :with_last_ticker, -> {
     where.not(last_ticker_id: nil)
   }
