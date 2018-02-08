3e69dad2a5056fa112a1375b61e3038a55c3c37f
Olivier Ducroux <olivier@dmates.io>
2018-02-08 19:10:50 +0100

    log size

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.logger = ActiveSupport::Logger.new(Rails.root.join('log', "#{Rails.env}.log"), 1, 50 * 1024 * 1024)
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
