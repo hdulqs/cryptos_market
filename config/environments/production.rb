c54906ff4a4f0178c50acb4a9736326962053b4c
Olivier Ducroux <olivier@dmates.io>
2018-02-08 02:32:45 +0100

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
     # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
 
+  config.action_mailer.default_url_options = { host: 'cryptosmarket.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
@@ -12,9 +13,6 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
-
-  config.action_mailer.default_url_options = { :host => 'cryptosmarket.io' }
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
