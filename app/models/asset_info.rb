20c4fac005f21e38a53b801c737141e815dd11e9
Olivier Ducroux <olivier@dmates.io>
2018-02-09 20:42:40 +0100

    missing png

--- app/models/asset_info.rb
+++ app/models/asset_info.rb
@@ -1,5 +1,5 @@
 class AssetInfo < ApplicationRecord
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "20x20>" }, default_url: "/images/:style/missing.png"#,
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "20x20>" }, default_url: "/system/images/:style/missing.png"#,
     # convert_options: {
     #   thumb: "-quality 100 -thumbnail 20x20"
     # }
--- app/models/market.rb
+++ app/models/market.rb
@@ -4,10 +4,10 @@ class Market < ApplicationRecord
   has_many :reports
   validates :name, uniqueness: true
 
-  has_attached_file :base_currency_logo, styles: { medium: "300x300>", thumb: "20x20>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :base_currency_logo, styles: { medium: "300x300>", thumb: "20x20>" }, default_url: "/system/images/:style/missing.png"
   validates_attachment_content_type :base_currency_logo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :quote_currency_logo, styles: { medium: "300x300>", thumb: "20x20>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :quote_currency_logo, styles: { medium: "300x300>", thumb: "20x20>" }, default_url: "/system/images/:style/missing.png"
   validates_attachment_content_type :quote_currency_logo, content_type: /\Aimage\/.*\z/
 
   scope :of_interest, -> {
