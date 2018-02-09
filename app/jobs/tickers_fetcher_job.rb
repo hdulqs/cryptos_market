971c9a116aecdd6ffc47a6f611a5103fac480863
Olivier Ducroux <olivier@dmates.io>
2018-02-09 17:55:15 +0100

    tickers

--- app/jobs/tickers_fetcher_job.rb
+++ app/jobs/tickers_fetcher_job.rb
@@ -4,8 +4,8 @@ class TickersFetcherJob < ApplicationJob
   def perform exchange_id
     exchange = Exchange.find(exchange_id)
     exchange.get_tickers
-    random_sec = Random.rand(1..30)
-    next_request = DateTime.current + (80 + random_sec).seconds
+    random_sec = Random.rand(1..50)
+    next_request = DateTime.current + (90 + random_sec).seconds
     TickersFetcherJob.set(wait_until: next_request).perform_later(exchange_id)
   end
 
